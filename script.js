06209be43b83cc464e065a2978804b25798b738e
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 09:27:15 -0300

    front-web

---
+++ script.js
@@ -0,0 +1,25 @@
+let nome = "henrique";
+let idade = 19;
+let status = true ;
+let valorProduto = 10.50 ;
+let listaTimes = ["Corinthians", "Palmeiras", "Santos"];
+
+console.log(nome);
+console.log(idade);
+console.log(status);
+console.log(parseFloat(valorProduto));
+console.log(listaTimes);
+
+
+let num1 = 10;
+let num2 = 5;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2
+let divisao = num1 / num2
+
+console.log("O resultado da soma é: " + soma);
+console.log("O resultado da subtração é: " + subtracao);
+console.log("O resultado da multiplicação é: " + multiplicacao);
+console.log("O resultado da divisão é: " + divisao);
